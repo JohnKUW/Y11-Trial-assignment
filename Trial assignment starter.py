bc778b166b2f3dc18d7a4b4a89ac5d99627810f2
John Leonard <john@johnleonard.me.uk>
2020-04-02 11:39:00 +0300

    Add files via upload

---
+++ Trial assignment starter.py
@@ -0,0 +1,21 @@
+# This is a trial assignment to test out how we can use Github
+# Don't worry about messing up!
+
+# Iteration
+
+# Write a for loop that asks the user for a number and outputs the times table
+# for that number up to 10 times
+# Use the variable identifier num1 for this program
+
+
+
+
+
+# Selection
+
+# Use an IF selection structure to check that a number meets certain requirements
+# Ask for a number and store it in a variable with the identifier num2
+# if the number is less than 7 output the text "Too low!"
+# if the number is 7 then output the text "Just right!"
+# if the number is more than 7 output the text "Too big!"
+
